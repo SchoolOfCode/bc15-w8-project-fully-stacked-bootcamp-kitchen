5b85d017272b1ba4f05318e596e02c9bd6c515d8
chisfy <fyzac96@gmail.com>
2023-10-25 11:50:11 +0100

    card created

--- client/app.js
+++ client/app.js
@@ -1,10 +1,10 @@
 document.querySelector(".feed").innerHTML += `
 
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+<div class="card container">
+  <img src="https://images.immediate.co.uk/production/volatile/sites/30/2023/06/Ultraprocessed-food-58d54c3.jpg?quality=90&resize=440,400" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="#" class="btn">Go somewhere</a>
   </div>
 </div>`
