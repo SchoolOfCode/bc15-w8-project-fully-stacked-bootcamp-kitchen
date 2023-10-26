2c4599da33762ed71d9178d2467ba6f5c9bc0e41
Christener19 <christenerleigh7070@gmail.com>
2023-10-26 14:49:48 +0100

    playwright 2 steps added

--- tests/play.test.js
+++ tests/play.test.js
@@ -2,8 +2,14 @@ import { test, expect } from "@playwright/test";
 //Number 1
 test("testing testing", async function ({page}) {
 
-    await page.goto("http://127.0.0.1:5501/client/index.html");
+    await page.goto("http://127.0.0.1:5502/client/index.html");
 
     const locateHeading = page.getByRole("heading", "text=Find 15 Minute Meals. Fast.")
     await expect(locateHeading).toBeVisible();
+
+    const searchInput = page.locator(`input[placeholder="search meal"]`);
+
+    await searchInput.type("Chicken");
+    await expect(searchInput).toHaveValue("Chicken");
+    await page.getByRole("button", { name: "Search" }).click();
 });
