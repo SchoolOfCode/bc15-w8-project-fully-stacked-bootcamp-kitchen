216e06e1baad0c66c80a0a0b044a2b3fe591d5bb
Quinnlizzy <quin27@hotmail.co.uk>
2023-10-26 13:57:38 +0100

    playwrightworking

--- tests/play.test.js
+++ tests/play.test.js
@@ -2,5 +2,8 @@ import { test, expect } from "@playwright/test";
 //Number 1
 test("testing testing", async function ({page}) {
 
-    await page.goto("http://localhost:3000/dishes");
-})
+    await page.goto("http://127.0.0.1:5501/client/index.html");
+
+    const locateHeading = page.getByRole("heading", "text=Find 15 Minute Meals. Fast.")
+    await expect(locateHeading).toBeVisible();
+});
