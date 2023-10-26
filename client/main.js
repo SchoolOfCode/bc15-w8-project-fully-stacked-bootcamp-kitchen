d7aba09f30d28bf5c30716c4782fb65bf75ad608
jjw9494 <joejameswalsh94@gmail.com>
2023-10-26 14:18:10 +0100

    Added responsive elements

--- client/main.js
+++ client/main.js
@@ -38,12 +38,13 @@ function handleToggle() {
 let searchBox = document.getElementById("search-input");
 // let btn = document.getElementById("search-click"); <--- If event input causing issues we can change searchBox to btn and input to click to only re-render on click
 let searchText = "";
-
+renderToDom();
 //Event listener to re-render Dom on input change
 searchBox.addEventListener("input", () => {
 	searchText = searchBox.value;
 	clearDom();
 	renderToDom();
+	console.log(searchText.split(" "));
 });
 
 //Clear te DOM before re-rendering new obj
@@ -53,18 +54,22 @@ function clearDom() {
 
 // Print card elements to the DOM
 function renderToDom() {
+	const searchWords = searchText.toLowerCase().split(" ");
+	
 	recipeArray
-		.filter(
-			(e) =>
-				e.title.toLowerCase().includes(`${searchText.toLowerCase()}`) ||
-				e.description.toLowerCase().includes(`${searchText.toLowerCase()}`) ||
-				e.tags.toLowerCase().includes(`${searchText.toLowerCase()}`)
-		)
+		.filter((e) => {
+			return searchWords.every(
+				(word) =>
+					e.title.toLowerCase().includes(word) ||
+					e.description.toLowerCase().includes(word) ||
+					e.tags.toLowerCase().includes(word)
+			);
+		})
 		.map((obj) => {
 			document.querySelector(".feed").innerHTML += `
 			<div class="row">
 			<div class="card container col-sm-10 ">
-			<img src="${obj.imageurl}" class="card-img-top" alt="...">
+			<img alt="${obj.title}"  src="${obj.imageurl}" class="card-img-top" alt="...">
 			<div class="card-body">
 			<h5 class="card-title">${obj.title}</h5>
 			<p class="card-text">${obj.description}</p>
