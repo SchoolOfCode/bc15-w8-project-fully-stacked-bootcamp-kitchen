c4db03f48a2950ba866cf61fac02a4e88724a853
Quinnlizzy <quin27@hotmail.co.uk>
2023-10-26 15:03:33 +0100

    playwrtightnearlydone

--- tests/play.test.js
+++ tests/play.test.js
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 //Number 1
 test("testing testing", async function ({page}) {
 
-    await page.goto("http://127.0.0.1:5502/client/index.html");
+    await page.goto("http://127.0.0.1:5501/client/index.html");
 
     const locateHeading = page.getByRole("heading", "text=Find 15 Minute Meals. Fast.")
     await expect(locateHeading).toBeVisible();
@@ -12,4 +12,8 @@ test("testing testing", async function ({page}) {
     await searchInput.type("Chicken");
     await expect(searchInput).toHaveValue("Chicken");
     await page.getByRole("button", { name: "Search" }).click();
+    await expect(page.getByText("Struggle to find dishes to cook that take very little time but the whole family ")).toBeVisible();
+    await page.getByRole("navigation", { name: "Path" }).click();
+    locateHeading = page.getByRole("heading", "text=Find 15 Minute Meals. Fast.")
+    await expect(locateHeading).toBeVisible();
 });
